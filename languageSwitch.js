3a0e2eb966f734d8aa5107f9fba61fbb48d9afa9
Andrew Savsunenko <andrew.savsunenko@icloud.com>
2024-12-09 21:17:47 +0200

    Update languageSwitch.js

--- languageSwitch.js
+++ languageSwitch.js
@@ -3,10 +3,12 @@ var lang = navigator.language;
 
 //checking if there was a "click"
 const ls = localStorage.hasOwnProperty('languageState');
+console.log("has State"+ls)
 
 if (lang=="uk-UA" && !ls){
     window.location.href = 'https://www.cmaukraine.com/ua';
     localStorage.setItem('languageState', 1);
+    console.log("State = " + localStorage.getItem('languageState'))
 }
 
 /*
