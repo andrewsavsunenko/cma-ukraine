3b322ffdd43b9a91d58eeb5fdb40e42800da0f97
Andrew Savsunenko <andrew.savsunenko@icloud.com>
2024-12-09 21:01:30 +0200

    Update languageSwitch.js

--- languageSwitch.js
+++ languageSwitch.js
@@ -6,19 +6,6 @@ const ls = localStorage.hasOwnProperty('languageState');
 
 if (lang=="uk-UA" && !ls){
     window.location.href = 'https://www.cmaukraine.com/ua';
-}
-
-//adding "click" on language switch
-const b1 = document.getElementById('eng1');
-const b2 = document.getElementById('eng2');
-const b3 = document.getElementById('eng3');
-
-function bClick(){
     localStorage.setItem('languageState', 1);
-    console.log("click Eng 1");
-    console.log(b1.innerText);
 }
 
-b1.addEventListener("click", bClick);
-b2.addEventListener("click", bClick);
-b3.addEventListener("click", bClick);
