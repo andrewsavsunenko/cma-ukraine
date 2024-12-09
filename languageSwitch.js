ca231f6ca57fe7c7a5b0bb8a7b1f9a741694f95f
Andrew Savsunenko <andrew.savsunenko@icloud.com>
2024-12-09 21:02:02 +0200

    Update languageSwitch.js

--- languageSwitch.js
+++ languageSwitch.js
@@ -9,3 +9,17 @@ if (lang=="uk-UA" && !ls){
     localStorage.setItem('languageState', 1);
 }
 
+//adding "click" on language switch
+const b1 = document.getElementById('eng1');
+const b2 = document.getElementById('eng2');
+const b3 = document.getElementById('eng3');
+
+function bClick(){
+    localStorage.setItem('languageState', 1);
+    console.log("click Eng 1");
+    console.log(b1.innerText);
+}
+
+b1.addEventListener("click", bClick);
+b2.addEventListener("click", bClick);
+b3.addEventListener("click", bClick);
