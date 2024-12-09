bd6607bcc136bd0940bfe479a9183ffb80ae3ec2
Andrew Savsunenko <andrew.savsunenko@icloud.com>
2024-12-09 19:19:28 +0200

    Update langSwitch.js

--- langSwitch.js
+++ langSwitch.js
@@ -1,10 +1,6 @@
-var lang = navigator.language;
+var lang = navigator.language.code;
 
-if (!$.cookie('language-uk-redirect-cookie') && lang.indexOf("uk") !== -1) {
-    var date = new Date();      
-    date.setTime(date.getTime() + 24 * 60 * 60 * 1000);
-    $.cookie('language-ua-redirect-cookie', true, { expires: date });
-    $(location).attr('href', 'https://www.cmaukraine.com/ua');
-} else {
-    // do nothing
+if (navigator.language.code=="uk"){
+    window.location.href = 'https://www.cmaukraine.com/ua';
 }
+    
