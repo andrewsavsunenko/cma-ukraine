dafdcf834ca84d3cde523496c585334ebecb2cdd
Andrew Savsunenko <andrew.savsunenko@icloud.com>
2025-03-11 21:19:24 +0200

    Update textLoadingAnimation.js

---
+++ textLoadingAnimation.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+   //gsap.registerPlugin(ScrollTrigger)
+    
+    let loopTml = gsap.timeline({repeat:-1, yoyo: true});
+    
+    loopTml.from("[title]", {
+        scale: 1.4,
+        duration: 0.5,
+        ease: "power2.out"
+        
+    })
+
+
+});
