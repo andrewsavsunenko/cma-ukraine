390ca2773f82591ad1ab21eecce5762736202187
Andrew Savsunenko <andrew.savsunenko@icloud.com>
2025-04-25 18:37:56 +0300

    .

--- scriptSwiperPartners.js
+++ scriptSwiperPartners.js
@@ -1,15 +1,16 @@
 const partnersSwiper = new Swiper(".swiper.partners", {
-    // Optional parameters
-  
-    slidesPerView: 1.6,
-    //slidesPerGroup: 2,
-    spaceBetween: 4,
-    loop: true,
-    allowTouchMove: false,
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: ".partners__arrow--wrapper.right",
-      prevEl: ".partners__arrow--wrapper.left",
-    },
-  });
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 1,
+  //slidesPerGroup: 2,
+  spaceBetween: 4,
+  loop: true,
+  allowTouchMove: false,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".partners__arrow--wrapper.right",
+    prevEl: ".partners__arrow--wrapper.left",
+  },
+});
