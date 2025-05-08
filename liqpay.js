6a8c1b7202c755d314c0a6722382aa2eb25664c6
Andrew Savsunenko <andrew.savsunenko@icloud.com>
2025-05-08 15:12:03 +0300

    updated and added liqpay.js

---
+++ liqpay.js
@@ -0,0 +1,53 @@
+class liqpayForm {
+  constructor() {
+    this.tabs = [...document.querySelectorAll(".donationTab")];
+    this.inputField = document.querySelector(".form__field.donationValue");
+    this.currencySelector = document.querySelector(
+      ".select__field.currencySelector"
+    );
+    this.donationButtons = [...document.querySelectorAll(".donationButton")];
+    this.submitButton = document.querySelector(".submitButton.liqpay");
+
+    this.currencyChange();
+    this.tabChange();
+    this.donationButtonClick();
+
+    this.formSubmit();
+    this.dataForm();
+    this.signatureForm();
+  }
+
+  currencyChange() {
+    this.currencySelector.addEventListener("change", (e) => {
+      const selectedCurrency = e.target.value;
+      this.donationButtons.forEach((btn) => {
+        btn.querySelector(".donationButtonCurrency").innerHTML =
+          selectedCurrency;
+      });
+    });
+  }
+
+  tabChange() {
+    tabs.forEach((tab) => {
+      tab.addEventListener("click", () => {
+        tabs.forEach((el) => el.classList.remove("clicked"));
+        tab.classList.add("clicked");
+      });
+    });
+  }
+
+  donationButtonClick() {
+    this.donationButtons.forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        const selectedValue = btn.querySelector(".donationValue").innerHTML;
+        this.inputField.value = selectedValue;
+      });
+    });
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const liqpay = new liqpayForm();
+});
+
+console.log("liqpay works");
--- localeSwitch.js
+++ localeSwitch.js
@@ -2,28 +2,11 @@
 var lang = navigator.language;
 
 //checking if there was a "click"
-const ls = localStorage.hasOwnProperty('languageState');
-console.log("has State "+ls)
+const ls = localStorage.hasOwnProperty("languageState");
+console.log("has State " + ls);
 
-if (lang=="uk-UA" && !ls){
-    window.location.href = 'https://www.cmaukraine.com/ua';
-    localStorage.setItem('languageState', 1);
-    console.log("State = " + localStorage.getItem('languageState'))
+if (lang == "uk-UA" && !ls) {
+  window.location.href = "https://www.cmaukraine.com/ua";
+  localStorage.setItem("languageState", 1);
+  console.log("State = " + localStorage.getItem("languageState"));
 }
-
-/*
-//adding "click" on language switch
-const b1 = document.getElementById('eng1');
-const b2 = document.getElementById('eng2');
-const b3 = document.getElementById('eng3');
-
-function bClick(){
-    localStorage.setItem('languageState', 1);
-    console.log("click Eng 1");
-    console.log(b1.innerText);
-}
-
-b1.addEventListener("click", bClick);
-b2.addEventListener("click", bClick);
-b3.addEventListener("click", bClick);
-*/
