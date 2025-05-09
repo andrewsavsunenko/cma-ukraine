4ca395167355bc6b1106c2b519daa87fa7c4a341
Andrew Savsunenko <andrew.savsunenko@icloud.com>
2025-05-09 13:44:46 +0300

    .

--- liqpay.js
+++ liqpay.js
@@ -7,15 +7,12 @@ class liqpayForm {
       ...document.querySelectorAll("[liqpayDonationButton]"),
     ];
     this.submitButton = document.getElementById("liqpaySubmitButton");
+    this.liqpayKeyFormHolder = document.getElementById("liqpayKeyFormHolder");
 
     this.currencyChange();
     this.tabChange();
     this.donationButtonClick();
-
-    //this.formSubmit();
-    //this.dataForm();
-    //this.signatureForm();
-
+    this.submitButtonClick();
     //this.log();
   }
 
@@ -55,6 +52,94 @@ class liqpayForm {
         const y = this.inputField.value;
         this.inputField.value = Number(x) + Number(y);
       });
+    });
+  }
+
+  /// Liqpay Build
+
+  async sha1(str) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(str);
+    const hashBuffer = await crypto.subtle.digest("SHA-1", data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray
+      .map((b) => b.toString(16).padStart(2, "0"))
+      .join("");
+    return hashHex;
+  }
+
+  getDate() {
+    var currentDate = new Date();
+    //console.log(currentDate.toUTCString());
+    var format = new Intl.DateTimeFormat("en", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: false,
+      timeZone: "UTC",
+    }).formatToParts(currentDate);
+
+    // year,month,day,hour,minute,second
+    var finalDate = [
+      format[4].value,
+      format[0].value,
+      format[2].value,
+      format[6].value,
+      format[8].value,
+      format[10].value,
+    ];
+
+    return finalDate.join("");
+  }
+
+  formGenerate() {
+    const activeTab = this.tabs.find((tab) =>
+      tab.classList.contains("clicked")
+    );
+    const tabValue = activeTab.getAttribute("liqpayTabAction");
+
+    // generating data and signature
+    json_string = JSON.stringify({
+      public_key: "sandbox_i34877559416",
+      version: "3",
+      action: tabValue, // pay or subsribe
+      amount: this.inputField.value, // amount
+      currency:
+        this.currencySelector.options[this.currencySelector.selectedIndex]
+          .innerHTML, //currency
+      description: "test",
+      order_id: getDate(), // order id
+    });
+
+    data = btoa(json_string);
+
+    sign_string =
+      "sandbox_YHV3x3Dx7oSe0jwtHmJI0SUmYyvhxP41USzDVIQk" +
+      data +
+      "sandbox_YHV3x3Dx7oSe0jwtHmJI0SUmYyvhxP41USzDVIQk";
+
+    signature = btoa(sha1(sign_string));
+
+    //generating liqpay form
+    const liqpayFormHTML = `
+      <form method="POST" action="https://www.liqpay.ua/api/3/checkout" accept-charset="utf-8">
+        <input type="hidden" name="data" value="${data}" />
+        <input type="hidden" name="signature" value="${signature}" />
+        <input type="image" src="//static.liqpay.ua/buttons/payUk.png" />
+      </form>
+    `;
+
+    this.liqpayKeyFormHolder.innerHTML = liqpayFormHTML;
+    this.liqpayKeyFormHolder.querySelector("form").submit();
+  }
+
+  submitButtonClick() {
+    this.submitButton.addEventListener("click", (btn) => {
+      btn.preventDefault();
+      this.formGenerate();
     });
   }
 }
--- liqpayTest.js
+++ liqpayTest.js
@@ -1,50 +1,52 @@
-const url='https://www.liqpay.ua/api/3/checkout;'
+const url = "https://www.liqpay.ua/api/3/checkout;";
 
-function liqpayGetRequest(){
-    const request = new Request(url,{
-        'version' : 3,
-        'public_key' : 'sandbox_i34877559416',
-        
-        'action' : 'pay',
-        'amount' : 500,
-        'currency' : USD,
-        
-        'order_id' : 'name here',
-        'description' : 'text here',
+function liqpayGetRequest() {
+  const request = new Request(url, {
+    version: 3,
+    public_key: "sandbox_i34877559416",
+
+    action: "pay",
+    amount: 500,
+    currency: USD,
+
+    order_id: "name here",
+    description: "text here",
 
     /*
         'subscribe_date_start' getDate(), //Date should be like 2015-03-31 00:00:00
         'subscribe_periodicity' : month,
     */
-        }
-    )
+  });
 }
 
+function getDate() {
+  var currentDate = new Date();
+  console.log(currentDate.toUTCString());
 
-function getDate(){
+  var format = new Intl.DateTimeFormat("en", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+    timeZone: "UTC",
+  }).formatToParts(currentDate);
 
-    var currentDate = new Date();	
-    console.log(currentDate.toUTCString());
-    
-    var format = new Intl.DateTimeFormat("en", {
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-        hour12: false,
-        timeZone: "UTC"
-        }).formatToParts(currentDate);
-    
-    var finalDate = [format[4].value,"-",format[0].value,"-",format[2].value," ",format[6].value,":",format[8].value,":",format[10].value];
-    
-return finalDate.join("")
+  // year,month,day,hour,minute,second
+  var finalDate = [
+    format[4].value,
+    format[0].value,
+    format[2].value,
+    format[6].value,
+    format[8].value,
+    format[10].value,
+  ];
 
+  return finalDate.join("");
 }
-    console.log(getDate())
-
-
+console.log(getDate());
 
 /*
 public_key = sandbox_i34877559416;
@@ -58,5 +60,3 @@ result_url= String
 переадресовано після завершення покупки.
 
 */
-
-
