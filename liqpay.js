6453704238b3eb5aa03ba94755754a4d6edf319e
Andrew Savsunenko <andrew.savsunenko@icloud.com>
2025-05-09 17:05:58 +0300

    .

--- liqpay.js
+++ liqpay.js
@@ -136,8 +136,8 @@ class liqpayForm {
       </form>
     `;
 
-    this.liqpayKeyFormHolder.innerHTML = liqpayFormHTML;
-    this.liqpayKeyFormHolder.querySelector("form").submit();
+    //this.liqpayKeyFormHolder.innerHTML = liqpayFormHTML;
+    //this.liqpayKeyFormHolder.querySelector("form").submit();
   }
 
   submitButtonClick() {
