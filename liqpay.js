d6a93281cbefd3a15c33cf1d11467e7ec15a7055
Andrew Savsunenko <andrew.savsunenko@icloud.com>
2025-05-16 13:34:29 +0300

    liqpay finalé

--- liqpay.js
+++ liqpay.js
@@ -139,7 +139,7 @@ class liqpayForm {
     // generating data and signature
 
     const json_string = JSON.stringify({
-      public_key: "sandbox_i34877559416", //i69188530090
+      public_key: "i69188530090", //sandbox_i34877559416
       version: "3",
       action: tabValue, // pay or subscribe
       amount: this.inputField.value, // amount
@@ -160,11 +160,11 @@ class liqpayForm {
     const data = btoa(json_string);
     //console.log("data - " + data);
 
-    //iOEJDUxsal5ZbNSIXVGo4Z0hxwM8GfnA0uiQ6Yxt
+    //sandbox_YHV3x3Dx7oSe0jwtHmJI0SUmYyvhxP41USzDVIQk
     const sign_string =
-      "sandbox_YHV3x3Dx7oSe0jwtHmJI0SUmYyvhxP41USzDVIQk" +
+      "iOEJDUxsal5ZbNSIXVGo4Z0hxwM8GfnA0uiQ6Yxt" +
       data +
-      "sandbox_YHV3x3Dx7oSe0jwtHmJI0SUmYyvhxP41USzDVIQk";
+      "iOEJDUxsal5ZbNSIXVGo4Z0hxwM8GfnA0uiQ6Yxt";
 
     //console.log("sign_string - " + sign_string);
     const signature = btoa(this.sha1(sign_string));
